0e2ed1b150afd9032aa57668adaa77683278c9ae
ianknowles <ian@imknowles.co.uk>
2020-02-28 22:21:56 +0000

    update the download link for the json file

--- public/javascripts/time-series.js
+++ public/javascripts/time-series.js
@@ -21,6 +21,7 @@ function change_report() {
 }
 
 function fetch_json() {
+	d3.select(`#${mapName}-data-link`).attr('href', `/assets/data/${timeseriesfile}`)
 	d3.select(`#${mapName}-shade`).attr('class', 'shade shade-on background-color-sea')
 	d3.json(`/assets/data/${timeseriesfile}`).then(timeseries);
 }
