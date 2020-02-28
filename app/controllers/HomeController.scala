82c8e714b5fd941caa615d7bec36cfecbe862467
ianknowles <ian@imknowles.co.uk>
2020-02-28 22:20:02 +0000

    add new views to the routes and controller

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,6 +1,7 @@
 package controllers
 
 import javax.inject._
+import models.Team
 import play.api._
 import play.api.i18n._
 import play.api.mvc._
@@ -22,4 +23,20 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents)(i
 	def index(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
 		Ok(views.html.index())
 	}
+
+	def dash(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+		Ok(views.html.dashboard())
+	}
+
+	def about(): Action[AnyContent] = TODO
+
+	def project(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+		Ok(views.html.project())
+	}
+
+	def team(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+		Ok(views.html.team(Team.people))
+	}
+
+	def privacy(): Action[AnyContent] = TODO
 }
