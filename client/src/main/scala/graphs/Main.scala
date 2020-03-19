521cad4dd6761978101c5a82b6684e042660b2a7
ianknowles <ian@imknowles.co.uk>
2020-03-19 17:21:50 +0000

    reimplement scrollfix.js in scala.js

--- client/src/main/scala/graphs/Main.scala
+++ client/src/main/scala/graphs/Main.scala
@@ -2,10 +2,35 @@ package graphs
 
 import graphs.shared.SharedMessages
 import org.scalajs.dom
+import org.scalajs.dom.raw.{Element, Event, HTMLElement}
 
 object Main {
+	def anchorScroll(fragment: String) {
+		val amount: Int = dom.document.getElementById("nav-header").clientHeight
+		val ttarget: Element = dom.document.getElementById(fragment)
+		ttarget match {
+			case element: HTMLElement => if (element.offsetTop > 0) {
+					//$('html,body').animate({ scrollTop: ttarget.offset().top - amount }, 250);
+					//$(ttarget).animate({ scrollTop: ttarget.offset().top - amount }, 250);
+				val scrolloffset: Double = element.offsetTop - amount
+				dom.document.body.scrollTop = scrolloffset
+				dom.document.documentElement.scrollTop = scrolloffset
+			}
+			case _ => println(ttarget)
+		}
+	}
+
+	def scrollToWindowHash(event: Event) {
+		if (dom.window.location.hash != "") {
+			val fragment: String = dom.window.location.hash.substring(1)
+			anchorScroll(fragment)
+			event.preventDefault()
+		}
+	}
 
 	def main(args: Array[String]): Unit = {
-		dom.document.getElementById("scalajsShoutOut").textContent = SharedMessages.itWorks
+		//dom.document.getElementById("scalajsShoutOut").textContent = SharedMessages.itWorks
+		dom.window.onload = scrollToWindowHash
+		dom.window.onhashchange = scrollToWindowHash
 	}
 }
