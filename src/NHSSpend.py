76d789c898d4cf42d6b5419bbf8d3c24ac9daaac
crahal <charlierahal@gmail.com>
2021-06-22 15:06:23 +0100

    a couple of final programming changes

--- src/NHSSpend.py
+++ src/NHSSpend.py
@@ -4,9 +4,11 @@ import pandas as pd
 from datetime import datetime
 from scrape_and_parse_ccgs import scrape_ccg
 from scrape_and_parse_trusts import scrape_trust
+from scrape_and_parse_nhsengland import scrape_nhsengland
 from merge_and_evaluate_tools import merge_eval_scrape, merge_eval_recon
 from generate_output import output_for_dashboard
 from reconciliation import reconcile_general, reconcile_general_norm
+from make_final_datasets import make_final_datasets
 
 
 def start_banner():
@@ -64,10 +66,14 @@ if __name__ == '__main__':
                                                   'data_nhsccgs', 'cleaned'))
     cleanpath_trusts = os.path.abspath(os.path.join(__file__, '../..', 'data',
                                                   'data_nhstrusts', 'cleaned'))
+    cleanpath_nhsengland = os.path.abspath(os.path.join(__file__, '../..', 'data',
+                                                        'data_nhsengland', 'cleaned'))
     mergepath = os.path.abspath(os.path.join(__file__, '../..', 'data',
                                              'data_merge'))
     reconcilepath = os.path.abspath(os.path.join(__file__, '../..', 'data',
                                                  'data_reconciled'))
+    finalpath = os.path.abspath(os.path.join(__file__, '../..', 'data',
+                                             'data_final'))
     norm_path = os.path.abspath(os.path.join(__file__, '../..', 'data',
                                              'data_support', 'norm_dict.tsv'))
     logpath = os.path.abspath(os.path.join(__file__, '../..', 'logging'))
@@ -77,24 +83,28 @@ if __name__ == '__main__':
         if os.path.exists(path) is False:
             os.makedirs(path)
     logger = setup_logging(logpath)
-    ccg_df = pd.read_excel(os.path.abspath(
-                           os.path.join(__file__, '../..', 'data',
-                                        'data_support', 'ccg_list.xls')),
-                           encoding='latin-1')
-    trust_df = pd.read_excel(os.path.abspath(
-                             os.path.join(__file__, '../..', 'data',
-                                          'data_support', 'trust_list.xls')),
-                             encoding='latin-1')
+#    ccg_df = pd.read_excel(os.path.abspath(
+#                           os.path.join(__file__, '../..', 'data',
+#                                        'data_support', 'ccg_list.xls')),
+#                           encoding='latin-1')
+#    trust_df = pd.read_excel(os.path.abspath(
+#                             os.path.join(__file__, '../..', 'data',
+#                                          'data_support', 'trust_list.xls')),
+#                             encoding='latin-1')
 #    scrape_ccg(ccg_df, False, True)
 #    scrape_trust(trust_df, False, True)
+#    scrape_nhsengland(True, True)
 #    merge_eval_scrape(cleanpath_ccgs, mergepath, htmlpath,
 #                      datasummarypath, logpath, 'ccgs_')
 #    merge_eval_scrape(cleanpath_trusts, mergepath, htmlpath,
 #                      datasummarypath, logpath, 'trusts_')
+#    merge_eval_scrape(cleanpath_nhsengland, mergepath, htmlpath,
+#                      datasummarypath, logpath, 'nhsengland_')
 #    uniq_name = ['ccgs_unique_unmatched_suppliers.tsv',
-#                 'trusts_unique_unmatched_suppliers.tsv']
+#                 'trusts_unique_unmatched_suppliers.tsv',
+#                 'nhsengland_unique_unmatched_suppliers.tsv']
 #    reconcile_general(mergepath, reconcilepath, uniq_name)
 #    reconcile_general_norm(mergepath, reconcilepath, uniq_name, norm_path)
-    merge_eval_recon(reconcilepath, norm_path, datapath, mergepath, logpath)
-    output_for_dashboard(mergepath, dashboardpath) # @TODO: add in reconciles
-#    output_for_analysis() # @TODO
+#    merge_eval_recon(reconcilepath, norm_path, datapath, mergepath, logpath)
+#    make_final_datasets()
+    output_for_dashboard(finalpath, dashboardpath, mergepath)
